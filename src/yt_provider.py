ac2a75ea51285e36adc8a5f4cd79034b926194ce
kirisakiken <bezmican96@gmail.com>
2022-12-04 01:43:35 +0300

    feat(yt): add load streams and download streams methods

--- src/yt_provider.py
+++ src/yt_provider.py
@@ -1,11 +1,30 @@
-from pytube import YouTube
+from pytube import YouTube, StreamQuery
 
 
 class YtProvider:
     @staticmethod
-    def get_yt(url: str, progress_handler: classmethod, complete_handler: classmethod):
+    def get_yt(url: str, progress_handler, complete_handler):
         return YouTube(
             url=url,
             on_progress_callback=progress_handler,
             on_complete_callback=complete_handler,
         )
+
+    @staticmethod
+    def load_streams(yt: YouTube):
+        print("Loading streams. . .")
+        streams = yt.streams
+        video_streams = streams.filter(progressive=True, only_video=True, file_extension='mp4').order_by(
+            'resolution').desc()
+        audio_streams = streams.filter(only_audio=True, file_extension='mp4').order_by('abr').desc()
+        return streams, video_streams, audio_streams
+
+    @staticmethod
+    def download_by_itag(stream_query: StreamQuery, itag: int, output_path: str = "./"):
+        try:
+            stream_query.get_by_itag(itag).download(output_path=output_path)
+        except:
+            print("Download failed")
+            return False
+
+        return True
